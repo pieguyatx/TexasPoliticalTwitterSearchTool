8c9bfb0d436472626cad8c891f9924e7448c8f3b
Pius Wong <pius@pioslabs.com>
2021-04-15 19:03:56 -0500

    Topic buttons styled according to if they are selected or not

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import Search from './components/Search';
 import Topics from './components/Topics';
 import Locations from './components/Locations';
 import Tweets from './components/Tweets';
 import './App.css';
 
-export default () => {
+const App = () => {
+    const defaultTopic = ['voting']; // array of starting topic
+    const [selectedTopics,setSelectedTopics] = useState(defaultTopic);
+
+    useEffect(() => {
+        console.log('"Topics" list updated...'); // debug
+    },[selectedTopics]);
+
     return (
         <div className="main ui container">
             <h1>Texas Political Twitter Search Tool</h1>
             <p>by Pius Wong</p>
-            <Topics />
+            <Topics selectedTopics={selectedTopics} setSelectedTopics={setSelectedTopics} />
             <Locations />
             <Search inputTerm="vote"/>
             <Tweets />
         </div>
     );
 }
+
+export default App;
--- src/components/Topics.js
+++ src/components/Topics.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import topics from '../data/topics';
 
-const Topics = () => {
+const Topics = ({selectedTopics, setSelectedTopics}) => {
+
+    const updateSelectedTopics = (e) =>{
+        console.log("Updating selected topic array: "); // debug
+
+    };
 
     const TopicList = topics.map( (item, index) => {
+        // console.log("Displaying buttons... Scanning for:", item[0]); // debug
+        let buttonColor = "gray";
+        const isInSelectedTopicsList = (selectedTopic) => selectedTopic===item[0];
+        if(selectedTopics.findIndex(isInSelectedTopicsList) > -1){
+            // console.log("Topic is already found among the selected topics!"); // debug
+            buttonColor = "green";
+        }
+        console.log(buttonColor);
         return (
-            <button key={index} className="topic-item ui button green">{ item[0] }</button>
+            <button 
+                key={index} 
+                className={`topic-item ui button ${buttonColor}`}
+                onClick={updateSelectedTopics}
+            >
+                { item[0] }
+            </button>
         );
     });
 
