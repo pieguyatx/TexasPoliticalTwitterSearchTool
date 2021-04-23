2a3e6dfab328b33bb896aa231704a085ccc36fdc
Pius Wong <pius@pioslabs.com>
2021-04-23 13:15:34 -0500

    Example data files for locations and topics added for archival

---
+++ src/example_locations.js
@@ -0,0 +1,12 @@
+// Put a file named "locations.js" in a folder called "data" inside "src".
+// Format:
+// location name, string describing latitude, longitude, and radius to look from there (25mi max)
+// See: https://www.latlong.net/
+
+const locations = [
+    ['Austin', '30.30086,-97.78262,25mi'],
+    ['San Antonio', '29.424122,-98.493629,25mi'],
+    ['Texarkana','33.442211,-94.064079,25mi']
+]
+
+export default locations;
---
+++ src/example_topics.js
@@ -0,0 +1,14 @@
+// Put a file named "topics.js" in a folder called "data" inside "src".
+// Format:
+// main topic search term, and related terms to search for
+
+const topics = [
+    [
+        "voting","vote","voted","election","early voting"
+    ],
+    [
+        "protest","rally","boycott","vigil" 
+    ]
+]
+
+export default topics;
